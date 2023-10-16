d24336cbdeb8edfb22c1af1f12abd2c6eae96008
Nicolas <nicolas.quoirez@hotmail.fr>
2023-10-16 14:18:16 -0700

    modification of file for the declaration of function print_alphabet "main.h -> file1"

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,14 +2,29 @@
 
 /**
  * main - Entry
- * the function print the alphabet
- * * * in lowercase
- * * followed by a new line
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	print_alphabet();
+	return (0);
+}
+
+
+/**
+ * print_alphabet
+ * function for print the alphabet in lowercase
+ *
+ */
+int print_alphabet(void)
+{
+	char count;
+
+	for (count = 'a'; count <= 'z'; count++)
+	{
+		_putchar(count);
+	}
+	_putchar(10);
 	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,20 +2,3 @@
 
 int _putchar(char);
 int print_alphabet(void);
-
-/**
- * print_alphabet 
- * function for print the alphabet in lowercase
- *
- */
-int print_alphabet(void)
-{
-	char count;
-
-	for (count = 'a'; count <= 'z'; count++)
-	{
-		_putchar(count);
-	}
-	_putchar(10);
-	return (0);
-}
