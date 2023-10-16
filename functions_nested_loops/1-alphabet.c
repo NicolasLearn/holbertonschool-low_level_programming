ff8c9d669d63d7f221435048555b11ad40997474
Nicolas <nicolas.quoirez@hotmail.fr>
2023-10-16 14:10:15 -0700

    writing script for file 1 and creation of the prototype and the function in main.h

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * the function print the alphabet
+ * * * in lowercase
+ * * followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,3 +1,21 @@
 #include <unistd.h>
 
 int _putchar(char);
+int print_alphabet(void);
+
+/**
+ * print_alphabet 
+ * function for print the alphabet in lowercase
+ *
+ */
+int print_alphabet(void)
+{
+	char count;
+
+	for (count = 'a'; count <= 'z'; count++)
+	{
+		_putchar(count);
+	}
+	_putchar(10);
+	return (0);
+}
