2f517b4e759b71ea745bdfdb1df895c9fd831b52
Nicolas <nicolas.quoirez@hotmail.fr>
2023-10-17 12:36:12 -0700

    function writ file 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -6,24 +6,18 @@
 
 void jack_bauer(void)
 {
-	char h1, h2, m1, m2;
+	int hour, minute;
 
-	for (h1 = '0'; h1 <= '2'; h1++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (h2 = '0'; h2 <= '3'; h2++)
+		for (minute = 0; minute < 60; minute++)
 		{
-			for (m1 = '0'; m1 <= '5'; m1++)
-			{
-				for (m2 = '0'; m2 <= '9'; m2++)
-				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m2);
-					_putchar('\n');
-				}
-			}
+			_putchar('0' + hour / 10);
+			_putchar('0' + hour % 10);
+			_putchar(':');
+			_putchar('0' + minute / 10);
+			_putchar('0' + minute % 10);
+			_putchar('\n');
 		}
 	}
 }
