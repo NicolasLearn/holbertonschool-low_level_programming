1e3b6166f93a5476414ad0a4479f978197131e1b
Nicolas <nicolas.quoirez@hotmail.fr>
2023-10-17 10:31:04 -0700

    function writ for file 10 and writing all prototype for the project

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add 2 integers return result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: result of sum
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
