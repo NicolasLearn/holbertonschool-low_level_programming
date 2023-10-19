0f88a46e311e36959a46e6d7b871f9ae7c1a4a32
Nicolas <nicolas.quoirez@hotmail.fr>
2023-10-19 05:44:01 -0700

    writing function for file 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return: result of multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
