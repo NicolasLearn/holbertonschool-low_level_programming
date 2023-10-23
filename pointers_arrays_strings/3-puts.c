af98dbdd568a905b2721f7496c4154fb55076688
Nicolas <nicolas.quoirez@hotmail.fr>
2023-10-23 02:14:29 -0700

    writing function for file 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - print a string enter in parmater to stdout
+ * @str: parameter type char to bo printed
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
