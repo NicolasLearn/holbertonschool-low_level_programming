d16b3b3ea62e4e89c13d22d8b9f8deb2d426ce9a
Nicolas <nicolas.quoirez@hotmail.fr>
2023-11-02 01:55:15 -0700

    creation of all files for the project, and the README

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry function, print name of executable
+ * @argc: number of argument of executable
+ * @argv: array contain name arguments of executable
+ *
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[]);
+	return (0);
+}
