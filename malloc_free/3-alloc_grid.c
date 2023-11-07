2ef50c80db085af9c79ed3df7a7951d78eaefa0e
Nicolas <nicolas.quoirez@hotmail.fr>
2023-11-07 08:59:30 -0800

    writing function for file 3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return a pointer to a 2 dimensional array
+ *	and initialize all at 0
+ * @width: number of column
+ * @height: number of line
+ *
+ * Return: a pointer to a 2 dimensional array.
+ *	NULL if failed or width or height is <= 0.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **tab = NULL;
+	int *tabTemp = NULL;
+	int i, j;
+
+	tab = malloc(8 * height);
+	if (tab == NULL || width <= 0 || height <= 0)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		tabTemp = malloc(sizeof(int) * width);
+		if (tabTemp == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+			tabTemp[j] = 0;
+		tab[i] = tabTemp;
+	}
+	return (tab);
+}
