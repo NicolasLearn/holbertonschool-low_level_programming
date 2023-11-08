883744bfcc49efcfacd5050750b412764f7462da
Nicolas <nicolas.quoirez@hotmail.fr>
2023-11-08 06:48:01 -0800

    creation of directory project "C - more malloc, free"

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+/**
+ * malloc_checked
+ */
+void *malloc_checked(unsigned int b)
---
+++ more_malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/*PROTOTYPE FUNCTION _PUTCHAR*/
+int _putchar(char c);
+
+/*PROTOTYPE FUNCTIONS OF THE PROJECT*/
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
