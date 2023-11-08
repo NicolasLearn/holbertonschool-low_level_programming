842cb84ee15871c0eb7668c0a1772e6095a01aff
Nicolas <nicolas.quoirez@hotmail.fr>
2023-11-08 06:57:17 -0800

    writing function for file 0

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * malloc_checked
+ * malloc_checked - allocate memory of size in parameter
+ * @b: size in bytes of the array
+ *
+ * Return: the address of tab if sucess.
+ *	exit 48 if malloc fails.
  */
 void *malloc_checked(unsigned int b)
+{
+	char *tab = NULL;
+
+	if (b <= 0)
+		exit(98);
+	tab = malloc(b);
+	if (tab == NULL)
+		exit(98);
+	return (tab);
+}
