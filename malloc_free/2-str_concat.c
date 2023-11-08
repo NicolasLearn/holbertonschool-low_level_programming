da99516412083675c3c3950e1e028eb69584f0c5
Nicolas <nicolas.quoirez@hotmail.fr>
2023-11-08 09:33:00 -0800

    correction function for file 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,6 +14,10 @@ char *str_concat(char *s1, char *s2)
 	char *tab = NULL;
 
 	count = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (s1[count] != '\0')
 		count++;
 	for (i = 0; s2[i] != '\0'; i++)
