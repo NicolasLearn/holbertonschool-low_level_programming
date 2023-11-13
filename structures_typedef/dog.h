a20cf390437ce9fb7792c26fde99f1ef4c04dcbc
Nicolas <nicolas.quoirez@hotmail.fr>
2023-11-13 03:16:48 -0800

    writing the struct for task 0, file "dog.h"

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef __DOG_H__
+#define __DOG_H__
+
+/**
+ * struct dog - struct for informartion of a dog
+ * @name: name of the dof (type char *)
+ * @age: age of the dog (type float)
+ * @owner: owner of the dog (type char *)
+ *
+ * Description: the struct allowed to give information on a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
