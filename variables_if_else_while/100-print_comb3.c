39c4906a2dc68ec945dd14c56bc9c145d6521e70
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-11-18 18:29:27 +0000

    writing function to file 100

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * print all possible different combinaitons of two digits
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
