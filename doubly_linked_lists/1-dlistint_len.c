9fadc5e8ff28fa29f55e69b5193a2e3951ca7c66
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-11-28 11:03:24 +0000

    writing function to file 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked "list_t list".
+ * @h: adress of the head value of the list.
+ *
+ * Return: number of elements in a linked "list_t list".
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
