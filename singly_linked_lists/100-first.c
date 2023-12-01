ecedfb09ad26d641e41863b4e6169fc71a7e3d73
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-12-01 10:57:40 +0000

    writing function to file 100, add proto in lists.h

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * print_before_main - function __attribute__((constructor))
+ * that prints a string before the execution of main.
+*/
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -38,5 +38,5 @@ list_t *add_node(list_t **head, const char *str);
 	int _strlen(char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+void print_before_main(void) __attribute__((constructor));
 #endif
