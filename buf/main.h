4e9a888363da10d9542c70ddae56995030ba669f
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-12-05 23:49:22 +0000

    remove librairy main.h

---
+++ buf/main.h
@@ -0,0 +1,27 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/*---------------------------------------------------------------------------*/
+		/*LIBRAIRY*/
+/*---------------------------------------------------------------------------*/
+
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/*---------------------------------------------------------------------------*/
+		/*PROTOTYPE*/
+/*---------------------------------------------------------------------------*/
+
+ssize_t read_textfile(const char *filename, size_t letters);
+	int _putchar(char c);
+	ssize_t clear_func(char *buf, int fd);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+	int _strlen(char *s);
+	int close_func(int fd);
+void close_file(int f1, int f2);
+void error_file(char *argv, int exit_val);
+
+#endif
