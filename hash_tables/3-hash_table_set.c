7a0535c2f979331af9d1fe5a1a384174a022ff1a
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-12-06 18:46:59 +0000

    add check if key is same

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,13 +10,25 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_h_node;
+	hash_node_t *new_h_node, *buf_node;
 	unsigned long int h_index = 0;
 
 	if ((ht == NULL) || (ht->array == NULL) || (key == NULL))
 		return (0);
 	if (value == NULL)
 		value = "";
+	h_index = key_index((unsigned char *)key, ht->size);
+	buf_node = ht->array[h_index];
+	while (buf_node != NULL)
+	{
+		if ((strcmp(buf_node->key, key)) == 0)
+		{
+			free(buf_node->value);
+			buf_node->value = strdup(value);
+			return (1);
+		}
+		buf_node = buf_node->next;
+	}
 	new_h_node = malloc(sizeof(hash_node_t));
 	if (new_h_node == NULL)
 	{
@@ -24,20 +36,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_h_node = NULL;
 		return (0);
 	}
-	h_index = key_index((unsigned char *)key, ht->size);
 	new_h_node->key = strdup(key);
 	new_h_node->value = strdup(value);
-	if (ht->array[h_index] != NULL)
-	{
-		if (ht->array[0] == NULL)
-			new_h_node->next = NULL;
-		else
-			new_h_node->next = ht->array[0];
-		ht->array[0] = new_h_node;
-	} else
-	{
+	if (ht->array[h_index] == NULL)
 		new_h_node->next = NULL;
-		ht->array[h_index] = new_h_node;
-	}
+	else
+		new_h_node->next = ht->array[h_index];
+	ht->array[h_index] = new_h_node;
+
 	return (1);
 }
