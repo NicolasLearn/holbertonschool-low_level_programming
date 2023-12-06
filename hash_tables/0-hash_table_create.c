24ad39033dcda39df345794aa2d32d73b989fa79
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2023-12-06 14:29:39 +0000

    add node at each index of the array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned int i;
 	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
 	if (new_hash_table == NULL)
@@ -19,7 +20,17 @@ hash_table_t *hash_table_create(unsigned long int size)
 	else
 	{
 		new_hash_table->size = size;
-		new_hash_table->array = NULL;
+		new_hash_table->array = malloc(sizeof(hash_node_t *) * size);
+		if (new_hash_table->array == NULL)
+		{
+			free(new_hash_table->array);
+			free(new_hash_table);
+			new_hash_table = NULL;
+		} else
+		{
+			for (i = 0; i < size; i++)
+				new_hash_table->array[i] = NULL;
+		}
 	}
 	return (new_hash_table);
 }
