6a6d8a3b1893798fca40311413d3fbff0e8aa6e3
Nicolas Quoirez <nicolas.quoirez@hotmail.fr>
2024-01-17 12:00:21 +0000

    create directory and files for the projects

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches for a value in a sorted array of integers using
+ * the Linear search algorithm.
+ *
+ * @array: Pointer to the first element of the array to search in.
+ * @size: Number of elements in the array.
+ * @value: Value to be searched.
+ *
+ * Return: The index of value on success.
+ * -1 if not found or array == NULL.
+*/
+int linear_search(int *array, size_t size, int value)
+{
+
+}
